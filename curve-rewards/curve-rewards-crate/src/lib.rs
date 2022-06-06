43382625a18f78630aaa1729da9e01d4e57b9643
Saad Majeed <saad.majeed@scytalelabs.com>
2022-06-06 18:10:48 +0500

    curve rewards added

--- curve-rewards/curve-rewards-crate/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![no_std]
-
-extern crate alloc;
-
-mod curve_rewards;
-pub mod data;
-pub mod event;
-
-pub use curve_rewards::CURVEREWARDS;
--- curve-rewards/curve-rewards-tests/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(test)]
-pub mod curve_rewards_tests;
-
-#[cfg(test)]
-pub mod curve_rewards_instance;
